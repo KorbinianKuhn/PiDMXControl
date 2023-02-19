4733704c0137025bb92a0e7f0096f40a2240c981
Kobi <korbiniankuhn@posteo.de>
2023-02-19 15:31:56 +0000

    Fixes

--- backend/src/dmx/devices/devices.ts
+++ backend/src/dmx/devices/devices.ts
@@ -1,7 +1,7 @@
 import { VarytecGigabarHex } from './VarytecGigaBarHex';
 
 export const hex1 = new VarytecGigabarHex(1, 'hex-1');
-export const hex2 = new VarytecGigabarHex(9, 'hex-2');
-export const hex3 = new VarytecGigabarHex(19, 'hex-3');
-export const hex4 = new VarytecGigabarHex(29, 'hex-4');
-export const hex5 = new VarytecGigabarHex(39, 'hex-5');
+export const hex2 = new VarytecGigabarHex(10, 'hex-2');
+export const hex3 = new VarytecGigabarHex(20, 'hex-3');
+export const hex4 = new VarytecGigabarHex(30, 'hex-4');
+export const hex5 = new VarytecGigabarHex(40, 'hex-5');
